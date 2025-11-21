f2bbed2ab81d3d69dfa1473e6877611c265d3f3e
aliflazuardi <aliflazuardi250999@gmail.com>
2025-11-21 21:30:00 +0700

    feat: implement ai agent using single prompt from args

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from boot-dev-ai-agent!")
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
+args = sys.argv
+if len(args) < 2:
+    print("prompt not provided")
+    sys.exit(1)
 
-if __name__ == "__main__":
-    main()
+user_prompt = args[1]
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents=messages
+)
+print(response.text)
+
+print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+print(f'Response tokens: {response.usage_metadata.candidates_tokens_details[0].token_count}')
